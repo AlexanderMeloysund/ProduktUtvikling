38cb9dc732dfb7f284671b8584fb43d23e5b0904
AlexanderMeloysund <AlexanderMeloysund@users.noreply.github.com>
2020-03-29 23:47:00 +0200

    comments and formatting

--- PU/str_mm_ler/str_mm_ler.ino
+++ PU/str_mm_ler/str_mm_ler.ino
@@ -1,38 +1,39 @@
 #include "TM1637.h"
 
 volatile int NbTopsFan; //measuring the rising edges of the signal
-int Calc;
-int hallsensor = 2;    //The pin location of the sensor
+int flowRate;
+int flowSensor = 2;
 
 const int CLK = 7;
 const int DIO = 8;
 TM1637 tm1637(CLK, DIO);
 
-void rpm ()     //This is the function that the interupt calls
+// the flow sensor sends a signal at certain rotations of the internal wheel
+// rpm() interrupts and counts each time the flow sensor sends a signal
+void rpm ()  
 {
-    NbTopsFan++;  //This function measures the rising and falling edge of the hall effect sensors signal
+  NbTopsFan++; 
 }
-// The setup() method runs once, when the sketch starts
+
 void setup() //
 {
-    pinMode(hallsensor, INPUT); //initializes digital pin 2 as an input
-    Serial.begin(9600); //This is the setup function where the serial port is initialised,
-    attachInterrupt(0, rpm, RISING); //and the interrupt is attached
+  pinMode(flowSensor, INPUT);
+  Serial.begin(9600);
+  attachInterrupt(0, rpm, RISING); // Interrupts when the input raises
 
-    tm1637.init();
-    tm1637.set(BRIGHT_TYPICAL);
+  tm1637.init();
+  tm1637.set(BRIGHT_TYPICAL);
 }
-// the loop() method runs over and over again,
-// as long as the Arduino has power
+
 void loop ()
 {
-    NbTopsFan = 0;   //Set NbTops to 0 ready for calculations
-    sei();      //Enables interrupts
-    delay (1000);   //Wait 1 second
-    cli();      //Disable interrupts
-    Calc = (NbTopsFan / 5); //(Pulse frequency x 60) / 5.5Q, = flow rate in L/hour
-    Serial.print (Calc, DEC); //Prints the number calculated above
-    Serial.print (" L/min\r\n"); //Prints "L/hour" and returns a  new line
-    tm1637.displayNum(Calc);
-    //Serial.println(digitalRead(hallsensor));
+  NbTopsFan = 0;   //Set NbTops to 0 ready for calculations
+  sei();      //Enables interrupts
+  delay (1000);
+  cli();      //Disable interrupts
+  flowRate = (NbTopsFan / 5); //flow rate in L/hour. The constant 5 is based on experiments
+  Serial.print (flowRate, DEC);
+  Serial.print (" L/hour\r\n");
+
+  tm1637.displayNum(flowRate); // Displays the flow on the screen
 }
